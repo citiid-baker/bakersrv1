c61f6e97a95966dc2ffdeb6448d36713da6f51b8
Jacqui Keane <872885+jacquikeane@users.noreply.github.com>
2022-07-13 14:07:47 +0100

    Update setup_refs.sh

--- setup_refs.sh
+++ setup_refs.sh
@@ -1,19 +1,39 @@
-Parameterise by output loaction (here) and number of threads (p)
+#!/bin/bash
 
-//allow conda activate
+set -eu
+
+# Script to download high quality reference genomes from refseq and store centrally
+
+source $MINICONDA/etc/profile.d/conda.sh
+
+# Parameterise by output loaction (here) and number of threads (p)
+
+LOCATION=$1
+THREADS=$2
+
+cd $LOCATION
 mkdir refs
+
 conda activate ncbi-genome-download-0.3.1
-# -p will depend on threads available
-ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o here -H -P -p 50 -r 3 -m bacteria_references.tsv bacteria
-ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o /home/software/refs -H -P -p 50 -r 3 -m /home/software/viral_references.tsv viruses
-//human
-ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o here -H -P -p 50 -r 3 -m ref_table bacteria
-//mouse
-ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o here -H -P -p 50 -r 3 -m ref_table bacteria
-//fix reference names with non-standard characters e.g. (,)
-cat refs/*.tsv > references_table.tsv
-//create a reffind script
+# Download viral genomes
+ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o /home/software/refs -H -P -p THREADS -r 3 -m /home/software/viral_references.tsv viruses
 
+# Download a limited set of bacerial genomes
+GENOMES={Acinetobacter,Aspergillus,Aliivibrio,Bacteroides,Bordetella,Burkholderia,Candida,Chlamydia,Campylobacter,Citrobacter,Clostridium,[Clostridium],Corynebacterium,Coxiella,Elizabethkingia,[Enterobacter],Enterobacter,Enterobacteriaceae,Enterococcus,Escherichia,Haemophilus,Helicobacter,Klebsiella,Legionella,Leptospira,Moraxella,Morganella,Mycobacterium,Neisseria,Pneumocystis,Proteus,Pseudomonas,Salmonella,Serratia,Shigella,Staphylococcus,Stenotrophomonas,Streptococcus}
+for each genome in GENOMES
+ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o here -H -P -p $THREADS -r 3 -m bacteria_references.tsv bacteria
 
+# Download human genome
+ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o here -H -P -r 3 -m ref_table bacteria
 
-Acinetobacter,Aspergillus,Aliivibrio,Bacteroides,Bordetella,Burkholderia,Candida,Chlamydia,Campylobacter,Citrobacter,Clostridium,[Clostridium],Corynebacterium,Coxiella,Elizabethkingia,[Enterobacter],Enterobacter,Enterobacteriaceae,Enterococcus,Escherichia,Haemophilus,Helicobacter,Klebsiella,Legionella,Leptospira,Moraxella,Morganella,Mycobacterium,Neisseria,Pneumocystis,Proteus,Pseudomonas,Salmonella,Serratia,Shigella,Staphylococcus,Stenotrophomonas,Streptococcus
+# Download mouse genome
+ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o here -H -P -r 3 -m ref_table bacteria
+
+# Fix reference names with non-standard characters e.g. (),
+
+# Join all the tables into one table
+cat $LOCATION/refs/*.tsv > $LOCATION/refs/references.tsv
+
+//create a reffind script ?
+
+conda deactivate
