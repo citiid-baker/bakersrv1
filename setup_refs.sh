52fb87572f5925d801c3c4fc02b2198b01298a06
Jacqui Keane <872885+jacquikeane@users.noreply.github.com>
2022-07-15 14:38:25 +0100

    Update setup_refs.sh

--- setup_refs.sh
+++ setup_refs.sh
@@ -6,42 +6,64 @@ set -eu
 
 source $MINICONDA/etc/profile.d/conda.sh
 
-# Parameterise by output loaction (here) and number of threads (p)
+# Parameterise by output loaction and number of threads
 
-LOCATION=$1
+LOCATION=$1 # Assumes location exists and is writeable by user software 
 THREADS=$2
 
 cd $LOCATION
-mkdir refs
 
+# Activate conda environment with ncbi-genome downlaodera
 conda activate ncbi-genome-download-0.3.1
-# Download viral genomes
-ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o /home/software/refs -H -P -p THREADS -r 3 -m /home/software/viral_references.tsv viruses
 
-# Download a limited set of bacerial genomes
-genomes=("Acinetobacte" "Aspergillus" "Aliivibrio" "Bacteroides" "Bordetella" "Burkholderia" " Candida" "Chlamydia" "Campylobacter" "Citrobacter" "Clostridium" "[Clostridium]" "Corynebacterium" "Coxiella" "Elizabethkingia" "[Enterobacter]" "Enterobacte" "Enterobacteriaceae" "Enterococcus" "Escherichia" "Haemophilus" "Helicobacter" "Klebsiella" "Legionella" "Leptospira" "Moraxella" "Morganella" "Mycobacterium" "Neisseria" "Pneumocystis" "Proteus" "Pseudomonas" "Salmonella" "Serratia" "Shigella" "Staphylococcus" "Stenotrophomonas" "Streptococcus")
-for i in "${genomes[@]}"
+# Download all viral genomes
+ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o $LOCATION -H -P -p $THREADS -r 3 -m ${LOCATION}/viral_references.tsv viruses
+
+# Download a limited set of bacterial genomes
+# What to do about "Enterobacteriaceae" as this is a family not Genus
+bacterial_genomes=("Acinetobacter" "Aliivibrio" "Bacteroides" "Bordetella" "Burkholderia" "Campylobacter" "Chlamydia" "Citrobacter" "Clostridium" "[Clostridium]" "Clostridioides" "Corynebacterium" "Coxiella" "Elizabethkingia" "[Enterobacter]" "Enterobacter" "Enterococcus" "Escherichia" "Haemophilus" "Helicobacter" "Klebsiella" "Legionella" "Leptospira" "Moraxella" "Morganella" "Mycobacterium" "Neisseria" "Proteus" "Pseudomonas" "Salmonella" "Serratia" "Shigella" "Staphylococcus" "Stenotrophomonas" "Streptococcus" "Vibrio")
+for i in "${bacterial_genomes[@]}"
 do
    : 
    # do whatever on "$i" here
    print $i
-   ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o here -H -P -p $THREADS -r 3 -m $i_references.tsv -g $i
+   ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o $LOCATION -H -P -p $THREADS -r 3 -m ${LOCATION}/${i}_references.tsv -g $i -N bacteria
 done
 
-for each genome in GENOMES
-ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o here -H -P -p $THREADS -r 3 -m bacteria_references.tsv bacteria
+# Download a limited set of fungal genomes
+fungal_genomes=("Aspergillus" "Candida" "[Candida]" "Pneumocystis")
+for j in "${funga_genomes[@]}"
+do
+   : 
+   # do whatever on "$j" here
+   print $j
+   ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o $LOCATION -H -P -p $THREADS -r 3 -m ${LOCATION}/${j}_references.tsv -g $j -N fungi
+done
 
-# Download human genome
-ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o here -H -P -r 3 -m ref_table bacteria
+# Download a limited set of eukaryote genomes
+euk_genomes=("Homo sapiens" "Mus musculus")
+for k in "${euk_genomes[@]}"
+do
+   : 
+   # do whatever on "$k" here
+   print $k
+   ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o ${LOCATION} -H -P -p $THREADS -r 3 -m ${LOCATION}/${k}_references.tsv -g $k -N vertebrate_mammalian
+done
 
-# Download mouse genome
-ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o here -H -P -r 3 -m ref_table bacteria
-
-# Fix reference names with non-standard characters e.g. (),
+# Fix reference names with non-standard characters e.g. (),+
+# TODO
 
 # Join all the tables into one table
-cat $LOCATION/refs/*.tsv > $LOCATION/refs/references.tsv
+cat ${LOCATION}/*.tsv > ${LOCATION}/references.tsv
 
-//create a reffind script ?
+#create a reffind script ?
+# TODO
+echo "This is the reffind script" > reffind.sh
+grep "S74" Proteus_references.tsv |  awk -F"\t" 'BEGIN {print "\"Species\",\"Reference File\""} {print $10" "$11","$23}'
+
+mv reffind.sh /home/software/bin
+chmod 775 /home/software/bin/reffind.sh
+
+echo "Now set "${LOCATION} "back to be owned by root?"
 
 conda deactivate
