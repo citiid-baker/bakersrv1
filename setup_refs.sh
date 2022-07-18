ff8c854523e40ee3acbf89df0b7fcd9c1c84c413
Jacqui Keane <872885+jacquikeane@users.noreply.github.com>
2022-07-18 17:43:02 +0100

    Update setup_refs.sh

--- setup_refs.sh
+++ setup_refs.sh
@@ -62,37 +62,49 @@ cd $LOCATION
 conda activate ncbi-genome-download-0.3.1
 
 # Download all viral genomes
-#ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o $LOCATION -H -P -p $THREADS -r 3 -m ${LOCATION}/viral_references.tsv viral
+echo "Viral genomes"
+ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o $LOCATION -H -P -p $THREADS -r 3 -m ${LOCATION}/viral_references.tsv viral
 
 # Download a limited set of bacterial genomes
 # What to do about "Enterobacteriaceae" as this is a family not Genus
-#bacterial_genomes=("Acinetobacter" "Aliivibrio" "Bacteroides" "Bordetella" "Burkholderia" "Campylobacter" "Chlamydia" "Citrobacter" "Clostridium" "[Clostridium]" "Clostridioides" "Corynebacterium" "Coxiella" "Elizabethkingia" "[Enterobacter]" "Enterobacter" "Enterococcus" "Escherichia" "Haemophilus" "Helicobacter" "Klebsiella" "Legionella" "Leptospira" "Moraxella" "Morganella" "Mycobacterium" "Neisseria" "Proteus" "Pseudomonas" "Salmonella" "Serratia" "Shigella" "Staphylococcus" "Stenotrophomonas" "Streptococcus" "Vibrio")
-#for i in "${bacterial_genomes[@]}"
-#do
-#   : 
-#   # do whatever on "$i" here
-#   print $i
-#   ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o $LOCATION -H -P -p $THREADS -r 3 -m ${LOCATION}/${i}_references.tsv -g $i -N bacteria
-#done
+bacterial_genomes=("Acinetobacter" "Aliivibrio" "Bacteroides" "Bordetella" "Burkholderia" "Campylobacter" "Chlamydia" "Citrobacter" "Clostridium" "[Clostridium]" "Clostridioides" "Corynebacterium" "Coxiella" "Elizabethkingia" "[Enterobacter]" "Enterobacter" "Enterococcus" "Escherichia" "Haemophilus" "Helicobacter" "Klebsiella" "Legionella" "Leptospira" "Moraxella" "Morganella" "Mycobacterium" "Neisseria" "Proteus" "Pseudomonas" "Salmonella" "Serratia" "Shigella" "Staphylococcus" "Stenotrophomonas" "Streptococcus" "Vibrio")
+for i in "${bacterial_genomes[@]}"
+do
+   :
+   do whatever on "$i" here
+   echo $i
+   ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o $LOCATION -H -P -p $THREADS -r 3 -m ${LOCATION}/${i}_references.tsv -g $i -N bacteria
+done
 
 # Download a limited set of fungal genomes
-#fungal_genomes=("Aspergillus" "Candida" "[Candida]" "Pneumocystis")
-#for j in "${fungal_genomes[@]}"
-#do
-#   : 
-#   # do whatever on "$j" here
-#   print $j
-#   ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o $LOCATION -H -P -p $THREADS -r 3 -m ${LOCATION}/${j}_references.tsv -g $j -N fungi
-#done
+fungal_genomes=("Aspergillus" "Candida" "[Candida]")
+for j in "${fungal_genomes[@]}"
+do
+   :
+   # do whatever on "$j" here
+   echo $j
+   ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o $LOCATION -H -P -p $THREADS -r 3 -m ${LOCATION}/${j}_references.tsv -g $j -N fungi
+done
+
+# Special case for genomes where no complete references exist
+fungal_genomes_all=("Pneumocystis")
+for f in "${fungal_genomes_all[@]}"
+do
+   :
+   # do whatever on "$f" here
+   echo $f
+   ncbi-genome-download -s refseq -F fasta,gff -l all -o $LOCATION -H -P -p $THREADS -r 3 -m ${LOCATION}/${f}_references.tsv -g $f -N fungi
+done
 
 # Download human and mouse genomes
-#ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o ${LOCATION} -H -P -p $THREADS -r 3 -m ${LOCATION}/host_references.tsv -g "Homo sapiens,Mus musculus" -N vertebrate_mammalian
+echo "Homo sapiens and Mus musculus"
+ncbi-genome-download -s refseq -F fasta,gff -l complete,chromosome -o ${LOCATION} -H -P -p $THREADS -r 3 -m ${LOCATION}/host_refoerences.tsv -g "Homo sapiens,Mus musculus" -N vertebrate_mammalian
 
 # Fix reference names with non-standard characters e.g. (),+
 # TODO?
 
 # Join all the tables into one table
-cat ${LOCATION}/*.tsv > ${LOCATION}/references.tsv
+cat ${LOCATION}/*.tsv | sort | uniq > ${LOCATION}/references.tsv
 
 # Put full path to files in references.tsv (escape slashes in location)
 sed -i "s/.\/refseq/${LOCATION////\\/}\/refseq/g" ${LOCATION}/references.tsv
@@ -113,16 +125,16 @@ function help
 {
    # Display Help
    script=\$(basename \$0)
-   echo 
+   echo
    echo "usage: "\$script" [-h] -s search_term"
    echo
    echo "Searches for the location of reference genome files that match the supplied search term"
    echo
    echo "optional arguments:"
-   echo "  -h, --help                  show this help message and exit"
+   echo "  -h              show this help message and exit"
    echo
    echo "required arguments:"
-   echo "-s search_term reference genome files that match the supplied search term"
+   echo "-s search_term    reference genome files that match the supplied search term"
    echo
 }
 
@@ -149,12 +161,12 @@ while getopts "hs:" option; do
          exit;;
    esac
 done
-grep \$TERM ${LOCATION}/references.tsv | awk -F"\t" 'BEGIN {print "\"Species\",\"Reference File\""} {print \$10" "\$11","\$23}'
+grep -i \$TERM ${LOCATION}/references.tsv | awk -F"\t" 'BEGIN {print "\"Species\",\"Reference File\""} {print \$10" "\$11","\$23}'
 END
 
-#mv reffind.sh /home/software/bin
-#chmod 775 /home/software/bin/reffind.sh
+mv reffind.sh /home/software/bin
+chmod 775 /home/software/bin/reffind.sh
 
-echo "Now set "${LOCATION} "back to be owned by root?"
+echo "Now set "${LOCATION} "back to be owned by root and remove world write permissions"
 
 conda deactivate
