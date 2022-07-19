c4bf7f9c6f954bab9236c67e1eb601fa0b5b2f75
Jacqui Keane <872885+jacquikeane@users.noreply.github.com>
2022-07-19 17:33:14 +0100

    Create install_treemer.sh

---
+++ install_treemer.sh
@@ -0,0 +1,55 @@
+ 1392  conda create -n treemmer-0.3 numpy
+ 1393  conda activaye treemmer-0.3
+ 1394  conda activate treemmer-0.3
+ 1395  ls
+ 1396  conda install ete3
+ 1397  conda install matplotlib
+ 1398  conda install joblib
+ 1399  wget https://github.com/fmenardo/Treemmer/archive/refs/tags/v0.3.tar.gz
+ 1400  ls
+ 1401  cd ~
+ 1402  wget https://github.com/fmenardo/Treemmer/archive/refs/tags/v0.3.tar.gz
+ 1403  tar -xvf v0.3.tar.gz 
+ 1404  ls
+ 1405  cd Treemmer-0.3/
+ 1406  ls
+ 1407  python Treemmer.py 
+ 1408  python2 Treemmer.py 
+ 1409  python3 Treemmer.py 
+ 1410  python3 Treemmer_v0.2.py 
+ 1411  python3 Treemmer_v0.1_beta.py 
+ 1412  wget https://github.com/fmenardo/Treemmer/releases/download/v0.3/Treemmer_v0.3.py
+ 1413  ls
+ 1414  python3 Treemmer_v0.3.py 
+ 1415  echo $PATH
+ 1416  cp Treemmer_v0.3.py /home/software/miniconda/envs/treemmer-0.3/bin/
+ 1417  chmod 777 /home/software/miniconda/envs/treemmer-0.3/bin/Treemmer_v0.3.py 
+ 1418  Treemmer_v0.3.py 
+ 1419  python Treemmer_v0.3.py 
+ 1420  cd ..
+ 1421  python Treemmer_v0.3.py 
+ 1422  which Treemmer_v0.3.py 
+ 1423  less /home/software/miniconda/envs/treemmer-0.3/bin/Treemmer_v0.3.py
+ 1424  less /home/software/miniconda/envs/treemmer-0.3/bin/Treemmer_v0.3.py | grep Treemer
+ 1425  less /home/software/miniconda/envs/treemmer-0.3/bin/Treemmer_v0.3.py | grep Treemmer
+ 1426  cd Treemmer-0.3/
+ 1427  ls
+ 1428  python Treemmer.py
+ 1429  less Treemmer.py
+ 1430  diff Treemmer.py Treemmer_v0.3.py
+ 1431  python Treemmer_v0.3.py 
+ 1432  cd ../..
+ 1433  python Treemmer_v0.3.py 
+ 1434  python3 Treemmer_v0.3.py 
+ 1435  echo $PATH
+ 1436  ls -l /home/software/miniconda/envs/treemmer-0.3/bin
+ 1437  ls -l /home/software/miniconda/envs/treemmer-0.3/bin/Treemmer_v0.3.py
+ 1438  Treemmer_v0.3.py 
+ 1439  python
+ 1440  python Treemmer_v0.3.py 
+ 1441  Treemmer_v0.3.py
+ 1442  which Treemmer_v0.3.py 
+ 1443  nano /home/software/miniconda/envs/treemmer-0.3/bin/Treemmer_v0.3.py
+ 1444  Treemmer_v0.3.py 
+ 1445  nano /home/software/miniconda/envs/treemmer-0.3/bin/Treemmer_v0.3.py
+ 1446  Treemmer_v0.3.py 
